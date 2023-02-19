3878dde059a543fd8202b344c7abfb3890a94397
AMcD <122405828+j3kp123@users.noreply.github.com>
2023-02-19 05:17:57 -0500

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,15 @@
+
+#a statement that prints hello world
+print('Hello World!\nThree fun facts about me...')
+
+#print a value from a list populated with several values
+colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'black']
+print('My favorite color is:', (colors[1]))
+
+#print one value from a dictionary populated with two keys and two values
+meals = {'breakfast': 'cereal', 'lunch': 'soup and salad', 'dinner': 'burger and fries'}
+print('My favorite meal is:', (meals['dinner']))
+
+#print one value from a tuple with 4 values
+pet_peeves = ('the word moist', 'horror movies', 'loud chewing', 'snobby people')
+print('My biggest pet peeve is:', (pet_peeves[3]))
